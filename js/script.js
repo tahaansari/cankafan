04d568a2738771a3445d4ebc228af9e94178529f
taha ansari <taha.ansari@schbang.com>
2020-03-12 19:28:31 +0530

    added video section

--- js/script.js
+++ js/script.js
@@ -1,6 +1,27 @@
-$(document).ready(function(){
-    // alert('hello world')
-    $('.watchVideo').click(function(){
-        $(this).toggleClass('open');
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        //options here
+        autoScrolling:false,
+        // scrollHorizontally: true
+    });
+    //methods
+    // $.fn.fullpage.setAllowScrolling(false);
+    $('.faq-box__item').click(function(){
+        if($(this).hasClass('is-active') ){
+            $('.faq-box__item').removeClass('is-active');
+            $('.faq-box__answer').slideUp();
+            return false;
+        }
+        $('.faq-box__item').removeClass('is-active');
+        $(this).addClass('is-active');
+        $('.faq-box__answer').slideUp();
+        $(this).find('.faq-box__answer').slideDown();
     })
-})
+    $('.testi-slider').slick({
+        arrows:false,
+        dots: true
+    })
+    $('.v-slider').slick({
+        arrows:false
+    })
+});
