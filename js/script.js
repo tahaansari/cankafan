e3178a0b307bf26a26bc6dbc84043c6ae221b03b
taha ansari <taha.m.ansari@gmail.com>
2020-03-13 10:38:24 +0530

    added video slider

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     $('#fullpage').fullpage({
         //options here
         autoScrolling:false,
+        fitToSection:false
         // scrollHorizontally: true
     });
     //methods
@@ -9,19 +10,21 @@ $(document).ready(function() {
     $('.faq-box__item').click(function(){
         if($(this).hasClass('is-active') ){
             $('.faq-box__item').removeClass('is-active');
-            $('.faq-box__answer').slideUp();
+            $('.faq-box__answer').slideUp('fast');
             return false;
         }
         $('.faq-box__item').removeClass('is-active');
         $(this).addClass('is-active');
-        $('.faq-box__answer').slideUp();
-        $(this).find('.faq-box__answer').slideDown();
+        $('.faq-box__answer').slideUp('fast');
+        $(this).find('.faq-box__answer').slideDown('fast');
     })
     $('.testi-slider').slick({
         arrows:false,
         dots: true
     })
     $('.v-slider').slick({
-        arrows:false
+        // arrows:false
+        // prevArrow: $('.prev'),
+        // nextArrow: $('.next')
     })
 });
