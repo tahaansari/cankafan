33d149c4516f2cdd91112c28e9fe55670ef6efd4
taha ansari <taha.m.ansari@gmail.com>
2020-03-16 10:16:23 +0530

    aded fullpage js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $('#fullpage').fullpage({
         //options here
-        autoScrolling:false,
+        // autoScrolling:false,
         fitToSection:false
         // scrollHorizontally: true
     });
@@ -23,14 +23,31 @@ $(document).ready(function() {
         dots: true
     })
     $('.v-slider').slick({
-        // arrows:false
-        // prevArrow: $('.prev'),
-        // nextArrow: $('.next')
+        prevArrow: $('.prev'),
+        nextArrow: $('.next')
     })
-    $('.watch-video').click(function(){
+    $('.header__watch-video a').click(function(){
+        
+        if( $(this).hasClass('is-active') ){
+            // alert('redirect to video');
+            $.fn.fullpage.moveTo(4);
+            $(this).toggleClass('is-active');
+            return false;
+        }
         $(this).toggleClass('is-active');
     })
     $('.fayde__hotspot').click(function(){
-        alert('clicked');
+        $('.overlay').show();
+        var id = $(this).attr('id');
+        $('[data-popup='+ $(this).attr('id')+']').slideDown('fast');
     })
+    $('.faydepop__close-svg').click(function(){
+        $('.overlay').hide();
+        $('.faydepop').slideUp('fast');
+    })
+    $(document).keyup(function(e) {
+        if (e.keyCode === 27){
+            $('.faydepop__close-svg').click();   // esc
+        }
+    });
 });
